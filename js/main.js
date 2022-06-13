b9f1867b0d5602035c9434d4148b2adff834d129
Pavel B <wmpawka@gmail.com>
2022-06-13 17:01:14 +0300

    added slow scroll

--- js/main.js
+++ js/main.js
@@ -9,4 +9,13 @@ $(function() {
     $('.header__burger, .header__list').removeClass('active');
     $('body').removeClass('lock');
   });
+
+  $(".header__link, .logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
 });
